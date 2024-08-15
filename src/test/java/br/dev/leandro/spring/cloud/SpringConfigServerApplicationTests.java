800ebec17f3750ad886bf8880997e6cc24e79193
Leandro Silva <lsilva.info@gmail.com>
2024-08-15 11:40:39 -0300

    Merge remote-tracking branch 'origin/master'

--- src/test/java/br/dev/leandro/spring/cloud/SpringConfigServerApplicationTests.java
+++ src/test/java/br/dev/leandro/spring/cloud/SpringConfigServerApplicationTests.java
@@ -1,12 +1,15 @@
 package br.dev.leandro.spring.cloud;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = RabbitAutoConfiguration.class)
 class SpringConfigServerApplicationTests {
 
-//    @Test
+    @Test
     void contextLoads() {
     }
 
